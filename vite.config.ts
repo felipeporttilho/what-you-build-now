418c83080d6e1c5420504ec0cf4850ba4aab31ae
felipeporttilho <felipeporttilho@gmail.com>
2025-04-19 03:45:47 -0300

    ajustes de build e estrutura para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // ESSA LINHA resolve seu problema
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@lib": path.resolve(__dirname, "./src/lib"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
     outDir: "dist",
-    rollupOptions: {
-      input: path.resolve(__dirname, "public/index.html"),
-    },
   },
-})
+});
